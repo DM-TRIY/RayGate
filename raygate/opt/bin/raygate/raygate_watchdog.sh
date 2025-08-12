7e158ee0f5cb4129c9c22d0420650a126746ac59
DM-TRIY <example@gmail.com>
2025-08-12 12:59:44 +0300

    Small fixes in files compability with each other

--- raygate/opt/bin/raygate/raygate_watchdog.sh
+++ raygate/opt/bin/raygate/raygate_watchdog.sh
@@ -5,8 +5,7 @@ IFACE="__IFACE__"
 TUN_PORT=9999
 
 while true; do
-    if iptables -t nat -C PREROUTING -i "$IFACE" -p tcp -m set --match-set "$SET_NAME" dst --dport 443 -j REDIRECT --to-port $TUN_PORT 2>/dev/null; then
-    else
+    if ! iptables -t nat -C PREROUTING -i "$IFACE" -p tcp -m set --match-set "$SET_NAME" dst --dport 443 -j REDIRECT --to-port $TUN_PORT 2>/dev/null; then
         echo "[RAYGATE_WATCHDOG] Iptables rule missing, adding..."
         iptables -t nat -A PREROUTING -i "$IFACE" -p tcp -m set --match-set "$SET_NAME" dst --dport 443 -j REDIRECT --to-port $TUN_PORT
     fi
